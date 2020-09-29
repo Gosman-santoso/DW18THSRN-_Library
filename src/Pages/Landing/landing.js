658fb649ad2065d50f2f2e9571cb1e8cc52dfe9b
Gosman-santoso <gosmansantoso18@gmail.com>
2020-09-29 22:37:33 +0700

    add landing

---
+++ src/Pages/Landing/landing.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import './landing.css';
+
+const Landing = () => {
+    return(
+        <div className="box-landing">
+            <div className="vektor1">
+                <img src={require('./../../Components/img/Vector1.png')} alt=""/>
+            </div>
+            <header>
+                <img src={require('./../../Components/img/Icon.png')} alt=""/>
+                <h1>Lib'rary</h1>
+            </header>
+            <div className="cover">
+                <h1><i>Your</i> library anywhere</h1>
+                <p>Sign-up today and recieve unlimited access to all of your reading - share your book.</p>
+                
+                <div className="sign-button">
+                    <button className="active">Sign Up</button>
+                    <Link to="/login"><button>Sign In</button></Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Landing;
---
+++ src/Pages/Register/register.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import './../Login/login.css';
+
+function Register(){
+    return(
+        <div className="box-sign">
+            <div className="sign-in">
+                <h1>Sign Up</h1>
+                <form action="">
+                    <input type="text" name="id" id="id" placeholder="Email"/> <br/>
+                    <input type="text" name="password" id="password" placeholder="Password"/>
+                    <input type="text" name="fullName" id="fullName" placeholder="Full Name"/>
+                    
+                    
+                    
+                    <input type="text" name="phone" id="phone" placeholder="Phone"/>
+                    <input type="text" name="address" id="address" placeholder="Address"/>
+                    
+                    <button>Sign Up</button>
+                </form>
+                <p>Already have an account ? Klik <Link to="/login"><strong>Here</strong></Link></p>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
