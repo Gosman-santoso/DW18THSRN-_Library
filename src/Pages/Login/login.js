9b2f4864982353fb7791d10618bc61102adccdc4
Gosman-santoso <gosmansantoso18@gmail.com>
2020-09-30 08:05:43 +0700

    add landing

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+// import React from 'react';
+// import {Link} from 'react-router-dom';
 
-import './login.css';
+// import './login.css';
 
-function Login(){
-    return(
-        <div className="box-sign">
-            <div className="sign-in">
-                <h1>Sign In</h1>
-                <form action="">
-                    <input type="text" name="id" id="id" placeholder="Email"/> <br/>
-                    <input type="text" name="password" id="password" placeholder="Password"/>
-                    <button>Sign in</button>
-                </form>
-                <p>Don't have an account ? Klik <Link to="/register"><strong>Here</strong></Link></p>
-            </div>
-        </div>
-    );
-}
+// function Login(){
+//     return(
+//         <div className="box-sign">
+//             <div className="sign-in">
+//                 <h1>Sign In</h1>
+//                 <form action="">
+//                     <input type="text" name="id" id="id" placeholder="Email"/> <br/>
+//                     <input type="text" name="password" id="password" placeholder="Password"/>
+//                     <button>Sign in</button>
+//                 </form>
+//                 <p>Don't have an account ? Klik<strong>Here</strong></p>
+//                 {/* <p>Don't have an account ? Klik <Link to="/register"><strong>Here</strong></Link></p> */}
+//             </div>
+//         </div>
+//     );
+// }
 
-export default Login;
+// export default Login;
