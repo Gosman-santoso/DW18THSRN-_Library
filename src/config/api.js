7a7c9d2a65e857c0847d6dd335df1e0e4d9e908a
Gosman-santoso <gosmansantoso18@gmail.com>
2020-11-25 17:40:23 +0700

    v1.0.0

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 // create base URL
+// export const API = axios.create({
+//     baseURL: "http://localhost:5000/api/v1"
+// });
+
 export const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1"
+    baseURL: "https://app-our-library.herokuapp.com/api/v1"
 });
 
 export const setAuthToken = token => {
@@ -13,8 +17,14 @@ export const setAuthToken = token => {
     }
 };
 
+// export const urlAsset = {
+//     img: "http://localhost:5000/public/img/",
+//     file: "http://localhost:5000/public/files/",
+//     thumbnail: "http://localhost:5000/public/thumbnails/"
+// };
+
 export const urlAsset = {
-    img: "http://localhost:5000/public/img/",
-    file: "http://localhost:5000/public/files/",
-    thumbnail: "http://localhost:5000/public/thumbnails/"
+    img: "https://app-our-library.herokuapp.com/public/img/",
+    file: "https://app-our-library.herokuapp.com/public/files/",
+    thumbnail: "https://app-our-library.herokuapp.com/public/thumbnails/"
 };
